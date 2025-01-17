578296aa87688127b7406e2689dab8afa216a24f
f95f <95felipefernandes@gmail.com>
2025-01-17 00:05:12 -0300

    [feat] first commit

---
+++ src/main/java/com/persons/speax/SpeaxApplication.java
@@ -0,0 +1,13 @@
+package com.persons.speax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/persons/speax/SpeaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persons.speax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
